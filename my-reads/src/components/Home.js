7d2fe33ec49b78f7ef1a7d64094f130f6347cec4
ThanhBT <105102395+kjjjoP3@users.noreply.github.com>
2024-08-31 21:34:46 +0700

    update logic display books

--- my-reads/src/components/Home.js
+++ my-reads/src/components/Home.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import BookList from './BookList';
-import { Link } from 'react-router-dom';
-import { SHELVES } from '../constants/constants';
+import React from "react";
+import BookList from "./BookList";
+import { Link } from "react-router-dom";
+import { SHELVES } from "../constants/constants";
 
 const Bookshelf = ({ books, onChangeShelf }) => {
   return (
@@ -11,17 +11,27 @@ const Bookshelf = ({ books, onChangeShelf }) => {
           <h1>MyReads</h1>
         </div>
         <div className="list-books-content">
-          {SHELVES.map((shelf) => (
-            <div className="bookshelf" key={shelf.title}>
-              <h2 className="bookshelf-title">{shelf.name}</h2>
-              <div className="bookshelf-books">
-                <BookList
-                  books={books.filter((book) => book.shelf === shelf.title)}
-                  onChangeShelves={onChangeShelf}
-                />
+          {SHELVES.map((shelf) => {
+            const booksInShelf = books.filter(
+              (book) => book.shelf === shelf.title
+            );
+
+            return (
+              <div className="bookshelf" key={shelf.title}>
+                <h2 className="bookshelf-title">{shelf.name}</h2>
+                <div className="bookshelf-books">
+                  {booksInShelf.length > 0 ? (
+                    <BookList
+                      books={booksInShelf}
+                      onChangeShelves={onChangeShelf}
+                    />
+                  ) : (
+                    <p>No books</p>
+                  )}
+                </div>
               </div>
-            </div>
-          ))}
+            );
+          })}
         </div>
         <div className="open-search">
           <Link to="/search">Add a book</Link>
